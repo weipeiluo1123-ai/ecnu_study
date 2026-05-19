78738b0b9ae6f1412328dc64e6be9bd6ce42e88c
weipeiluo1123-ai <weipeiluo1123@outlook.com>
2026-05-19 22:07:16 +0800

    326 3的幂

--- algorithm/leetcode/326.power-of-three.cpp
+++ algorithm/leetcode/326.power-of-three.cpp
@@ -1,4 +1,4 @@
-/*
+326/*
  * @lc app=leetcode id=326 lang=cpp
  *
  * [326] Power of Three
@@ -8,7 +8,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
+        if(n<=0) return false;
+        while(n%3==0) n/=3;
+        return n==1;
     }
 };
 // @lc code=end
